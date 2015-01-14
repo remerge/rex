5a35e927c77d217944ec8bdcd4f6526352860524
Martin Karlsch <martin@karlsch.com>
2015-01-14 16:20:34 +0100

    add functions to remove accents from strings

---
+++ string.go
@@ -0,0 +1,96 @@
+package rex
+
+import "bytes"
+
+// from - https://github.com/kennygrant/sanitize/blob/master/sanitize.go
+
+// Replace a set of accented characters with ascii equivalents.
+func RemoveAccents(text string) string {
+	// Replace some common accent characters
+	b := bytes.NewBufferString("")
+	for _, c := range text {
+		// Check transliterations first
+		if val, ok := transliterations[c]; ok {
+			b.WriteString(val)
+		} else {
+			b.WriteRune(c)
+		}
+	}
+	return b.String()
+}
+
+// A very limited list of transliterations to catch common european names translated to urls.
+// This set could be expanded with at least caps and many more characters.
+var transliterations = map[rune]string{
+	'À': "A",
+	'Á': "A",
+	'Â': "A",
+	'Ã': "A",
+	'Ä': "A",
+	'Å': "AA",
+	'Æ': "AE",
+	'Ç': "C",
+	'È': "E",
+	'É': "E",
+	'Ê': "E",
+	'Ë': "E",
+	'Ì': "I",
+	'Í': "I",
+	'Î': "I",
+	'Ï': "I",
+	'Ð': "D",
+	'Ł': "L",
+	'Ñ': "N",
+	'Ò': "O",
+	'Ó': "O",
+	'Ô': "O",
+	'Õ': "O",
+	'Ö': "O",
+	'Ø': "OE",
+	'Ù': "U",
+	'Ú': "U",
+	'Ü': "U",
+	'Û': "U",
+	'Ý': "Y",
+	'Þ': "Th",
+	'ß': "ss",
+	'à': "a",
+	'á': "a",
+	'â': "a",
+	'ã': "a",
+	'ä': "a",
+	'å': "aa",
+	'æ': "ae",
+	'ç': "c",
+	'è': "e",
+	'é': "e",
+	'ê': "e",
+	'ë': "e",
+	'ì': "i",
+	'í': "i",
+	'î': "i",
+	'ï': "i",
+	'ð': "d",
+	'ł': "l",
+	'ñ': "n",
+	'ń': "n",
+	'ò': "o",
+	'ó': "o",
+	'ô': "o",
+	'õ': "o",
+	'ō': "o",
+	'ö': "o",
+	'ø': "oe",
+	'ś': "s",
+	'ù': "u",
+	'ú': "u",
+	'û': "u",
+	'ū': "u",
+	'ü': "u",
+	'ý': "y",
+	'þ': "th",
+	'ÿ': "y",
+	'ż': "z",
+	'Œ': "OE",
+	'œ': "oe",
+}
