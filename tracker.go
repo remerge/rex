cda06770421294d37df94ea710c538f041cae81e
Benedikt Böhm <bb@xnull.de>
2015-05-16 21:35:34 +0200

    expose tracker.AddMetadata

--- tracker.go
+++ tracker.go
@@ -2,6 +2,7 @@ package rex
 
 type Tracker interface {
 	Close()
+	AddMetadata(e EventBase, full bool)
 	FastMessage(topic string, message []byte)
 	FastEvent(topic string, event EventBase, metadata bool)
 	FastEventMap(topic string, event map[string]interface{}, metadata bool)
