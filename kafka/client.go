0f37327c1ff7a2f15ceb755a236d64106e275841
Martin Karlsch <martin@karlsch.com>
2016-06-27 15:53:34 +0200

    add offset map and kafka offset helper

--- kafka/client.go
+++ kafka/client.go
@@ -17,37 +17,68 @@ type Client struct {
 func NewClient(id string, broker_list string) (*Client, error) {
 	sarama.Logger = loggerWrapper{loggo.GetLogger("sarama")}
 
-	self := &Client{
+	client := &Client{
 		id:      id,
 		brokers: strings.Split(broker_list, ","),
 		log:     loggo.GetLogger("kafka.client." + id),
 	}
 
 	var err error
-	self.Client, err = sarama.NewClient(self.brokers, nil)
+	client.Client, err = sarama.NewClient(client.brokers, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	return self, nil
+	return client, nil
 }
 
-func (self *Client) GetId() string {
-	return self.id
+func (client *Client) GetId() string {
+	return client.id
 }
 
-func (self *Client) GetGroupOffset(group string, topic string, partition int32, offset int64) (int64, error) {
-	broker, err := self.Leader(topic, partition)
+// actually this returns the earliest or latest offsets for a topic and its partitions
+// the group is ignored atm - we need to implement proper offset commits and change the fucntion alter
+func (client *Client) GetGroupOffset(group string, topic string, partition int32, offset int64) (int64, error) {
+	return client.GetOffset(topic, partition, offset)
+}
+
+// func (client *Client) GetOffset(topic string, partition int32, offset int64) (int64, error) {
+// 	broker, err := client.Leader(topic, partition)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+
+// 	request := &sarama.OffsetRequest{}
+// 	request.AddBlock(topic, partition, offset, 100)
+// 	offsets, err := broker.GetAvailableOffsets(request)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+// 	block := offsets.GetBlock(topic, partition)
+// 	return block.Offsets[0], nil
+// }
+
+func (client *Client) GetOffsets(topic string) (earliestMap OffsetMap, latestMap OffsetMap, err error) {
+	earliestMap = make(OffsetMap)
+	latestMap = make(OffsetMap)
+	// TODO : optimize to get this earliest and latest with one kafka call
+	partitions, err := client.Partitions(topic)
 	if err != nil {
-		return 0, err
+		return nil, nil, err
 	}
 
-	request := &sarama.OffsetRequest{}
-	request.AddBlock(topic, partition, offset, 100)
-	offsets, err := broker.GetAvailableOffsets(request)
-	if err != nil {
-		return 0, err
+	for _, p := range partitions {
+		earliest, err := client.GetOffset(topic, p, sarama.OffsetOldest)
+		if err != nil {
+			return nil, nil, err
+		}
+		earliestMap[p] = earliest
+
+		latest, err := client.GetOffset(topic, p, sarama.OffsetNewest)
+		if err != nil {
+			return nil, nil, err
+		}
+		latestMap[p] = latest
 	}
-	block := offsets.GetBlock(topic, partition)
-	return block.Offsets[0], nil
+	return earliestMap, latestMap, nil
 }
