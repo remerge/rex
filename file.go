0a49f76c662333b63e631f096bcdd20c01846351
Martin Karlsch <martin@karlsch.com>
2016-06-28 15:35:43 +0200

    move copy file function to rex

---
+++ file.go
@@ -0,0 +1,31 @@
+package rex
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func CopyFile(src, dst string) (int64, error) {
+	src_file, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer src_file.Close()
+
+	src_file_stat, err := src_file.Stat()
+	if err != nil {
+		return 0, err
+	}
+
+	if !src_file_stat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	dst_file, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer dst_file.Close()
+	return io.Copy(dst_file, src_file)
+}
