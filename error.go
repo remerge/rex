9bdd432b8a8600528c7d8bbbd1ae783b47bca5da
Cris Mihalache <me@f3rno.com>
2016-09-19 14:00:37 +0000

    add CompositeError method, useful in defer blocks

--- error.go
+++ error.go
@@ -6,6 +6,24 @@ import (
 	"github.com/remerge/rex/rollbar"
 )
 
+// Returns a multi-line error containing all of the supplied errors, with nil
+// values excluded
+func CompositeError(errors ...error) error {
+	finalErrorString := ""
+
+	for i := 0; i < len(errors); i++ {
+		if errors[i] != nil {
+			if len(finalErrorString) > 0 {
+				finalErrorString += "\n"
+			}
+
+			finalErrorString += errors[i].Error()
+		}
+	}
+
+	return fmt.Errorf(finalErrorString)
+}
+
 func MayPanic(err error) {
 	if err != nil {
 		rollbar.Error(rollbar.CRIT, err)
