e4311bfc1880b046be6358f05c3e71f17cb79352
Cris Mihalache <me@f3rno.com>
2016-11-10 20:36:01 +0100

    add MergeErrors

--- error.go
+++ error.go
@@ -17,3 +17,10 @@ func MayPanic(err error) {
 func MayPanicNew(format string, v ...interface{}) {
 	MayPanic(fmt.Errorf(format, v...))
 }
+
+// Creates a new error with both messages seperated by a newline
+// Useful in defer blocks at the end of methods, where errors should still
+// propagate (always)
+func MergeErrors(a error, b error) error {
+	return fmt.Errorf("%s\n%s", a.Error(), b.Error())
+}
