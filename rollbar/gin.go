ebaf8e47047d36b7d1f48b3a01a2cb94dabb1da3
Alexander Dorofeev <aka.spin@gmail.com>
2018-09-11 19:57:09 +0200

    send all gin errors to rollbar except "invalid url escape"

--- rollbar/gin.go
+++ rollbar/gin.go
@@ -12,13 +12,19 @@ import (
 	"github.com/remerge/go-lock_free_timer"
 )
 
-var logger = loggo.GetLogger("gin")
+var (
+	logger         = loggo.GetLogger("gin")
+	ginErrPrefixes = []string{
+		"gin.Error: invalid URL escape",
+		"gin.Error: read tcp",
+	}
+)
 
 // LogMeteredError writes message to log as well as increases "track_error"
 // metric. First 32 runes of error are used for "reason" label in
 // URL-parametrised form. All extra params which begins from "m+" prefix will
 // be used as labels.
-func LogMeteredError(err error, extra ...string)  {
+func LogMeteredError(err error, extra ...string) {
 	// try to strip reason as possible
 	split := strings.SplitN(err.Error(), ":", 2)
 	labels := []string{fmt.Sprintf("reason=%.32s", inflect.Parameterize(split[0]))}
@@ -36,7 +42,7 @@ func LogMeteredError(err error, extra ...string)  {
 		}
 		line = append(line, v)
 	}
-	metrics.GetOrRegisterCounter("track,"+strings.Join(labels, ",") + " error", lft.DefaultRegistry).Inc(1)
+	metrics.GetOrRegisterCounter("track,"+strings.Join(labels, ",")+" error", lft.DefaultRegistry).Inc(1)
 	logger.LogCallf(3, loggo.ERROR, strings.Join(append(line, labels...), " "))
 }
 
@@ -56,11 +62,18 @@ func GinRecovery() gin.HandlerFunc {
 				return
 			}
 
+		errReportLoop:
 			for _, err := range c.Errors {
 				LogMeteredError(err,
 					"m+partner="+c.Request.Form.Get("partner"),
 					"url="+c.Request.URL.String(),
 					"remote_addr="+c.Request.RemoteAddr)
+				for _, prefix := range ginErrPrefixes {
+					if strings.HasPrefix(err.Error(), prefix) {
+						continue errReportLoop
+					}
+				}
+				RequestErrorWithStackSkip(ERR, c.Request, err, 3)
 			}
 
 			c.JSON(500, gin.H{
